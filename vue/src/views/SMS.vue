0783880fd97f954053641426581ece9dc6bd7d9a
Lowrlor <vicheslav.grabelnik@gmail.com>
2022-07-25 13:08:04 +0300

    update

---
+++ vue/src/views/SMS.vue
@@ -0,0 +1,17 @@
+<template>
+  <template>
+      <li v-for="(lang, k) in langs" :key="k">
+          <a role="button" v-on:click="$i18n.locale = k">asd</a>
+      </li>
+  </template>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'SmsPage',
+  components: {
+  }
+}
+</script>
